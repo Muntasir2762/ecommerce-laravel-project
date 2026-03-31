dc644255a5ec03479e2bb174d74f3d9833240f05
Munotasir2762 <muntasirpranto2762@gmail.com>
2026-04-01 00:18:40 +0600

    Home page mastering is done

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\Frontend\FrontendController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', [FrontendController::class, 'index']);
+Route::get('/product-details', [FrontendController::class, 'productDetails']);
