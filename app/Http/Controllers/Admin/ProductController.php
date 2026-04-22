a4030e98d54e84a7853386f5db15e661977d5b95
Munotasir2762 <muntasirpranto2762@gmail.com>
2026-04-22 23:22:45 +0600

    Validation rules-product | Product Create Page Update

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\Product;
 use App\Models\SubCategory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ProductController extends Controller
 {
@@ -14,5 +16,54 @@ class ProductController extends Controller
         $categories = Category::orderBy('name', 'asc')->get();
         $subCategories = SubCategory::orderBy('name', 'asc')->get();
         return view('admin.product.create', compact('categories', 'subCategories'));
+    }
+
+    public function store (Request $request)
+    {
+    
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'sku_code' => 'nullable|unique:products,sku_code',
+            'cat_id' => 'required|integer',
+            'subcat_id'=> 'required|integer',
+            'buying_price' => 'required',
+            'regular_price' => 'required',
+            'qty' => 'required|integer',
+            'product_type' => 'required|string|max:255',
+            'description' => 'required|string',
+            'image' => 'required|image|max:2048'
+        ],[
+            'name.required' => 'প্রোডাক্ট এর নাম জরুরি',
+            'name.max' => 'প্রোডাক্ট এর নাম সর্বোচ্চ 255 ক্যারেক্টর!',
+            'buying_price.required' => 'পণ্যের ক্রয় মূল্য জরুরি '
+        ]);
+
+        $product = new Product();
+
+        $product->name = $request->name;
+        $product->slug = Str::slug($request->name);
+        $product->sku_code = $request->sku_code;
+        $product->cat_id = $request->cat_id;
+        $product->subcat_id = $request->subcat_id;
+        $product->buying_price = $request->buying_price;
+        $product->regular_price = $request->regular_price;
+        $product->discount_price = $request->discount_price;
+        $product->qty = $request->qty;
+        $product->product_type = $request->product_type;
+        $product->description = $request->description;
+        $product->product_policy = $request->product_policy;
+
+        if(isset($request->image)){
+            $image = $request->file('image');
+            $imageName = rand().'.'.$image->getClientOriginalExtension(); //4347657.jpg
+            $image->move('admin/product', $imageName);
+
+            $product->image = url('admin/product/'.$imageName); //http://127.0.0.1:8000/admin/category/4347657.jpg
+        }
+
+        $product->save();
+
+        toastr()->success('Product created successfully');
+        return redirect()->back();
     }
 }
--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -47,26 +47,35 @@
                                         <div class="col-md-6 col-sm-12 col-12">
                                             <div class="mb-3">
                                                 <label for="name" class="form-label">Product Name</label>
-                                                <input type="text" class="form-control" name="name" id="name"
-                                                    required />
+                                                <input type="text" class="form-control" name="name" id="name" value="{{old('name')}}" />
+                                                 @error('name')
+                                                     <span class="text-danger">{{$message}}</span>
+                                                 @enderror
                                             </div>
                                         </div>
 
                                         <div class="col-md-6 col-sm-12 col-12">
                                             <div class="mb-3">
                                                 <label for="sku_code" class="form-label">Product SKU Code (Optional)</label>
-                                                <input type="text" class="form-control" name="sku_code" id="sku_code" />
+                                                <input type="text" class="form-control" name="sku_code" id="sku_code" value="{{old('sku_code')}}"/>
+                                                @error('sku_code')
+                                                     <span class="text-danger">{{$message}}</span>
+                                                 @enderror
                                             </div>
                                         </div>
 
                                         <div class="col-md-6 col-sm-12 col-12">
                                             <div class="mb-3">
                                                 <label for="cat_id" class="form-label">Select Category</label>
-                                                <select name="cat_id" id="cat_id" class="form-control" required>
+                                                <select name="cat_id" id="cat_id" class="form-control">
+                                                    <option value="" disabled selected>Select Category</option>
                                                     @foreach ($categories as $category)
                                                         <option value="{{ $category->id }}">{{ $category->name }}</option>
                                                     @endforeach
                                                 </select>
+                                                @error('cat_id')
+                                                     <span class="text-danger">{{$message}}</span>
+                                                 @enderror
                                             </div>
                                         </div>
 
@@ -74,74 +83,132 @@
                                             <div class="mb-3">
                                                 <label for="subcat_id" class="form-label">Select SubCategory (optional)</label>
                                                 <select name="subcat_id" id="subcat_id" class="form-control">
+                                                    <option value="" disabled selected>Select Subcategory</option>
                                                     @foreach ($subCategories as $subcategory)
                                                         <option value="{{ $subcategory->id }}">{{ $subcategory->name }}
                                                         </option>
                                                     @endforeach
                                                 </select>
+                                                @error('subcat_id')
+                                                     <span class="text-danger">{{$message}}</span>
+                                                 @enderror
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-6 col-sm-12 col-12">
+                                            <div class="mb-3" id="color_fields">
+                                                <label for="color_name" class="form-label">Color (Optional)</label>
+                                                <input type="text" class="form-control mb-2" name="color_name[]" placeholder="Color Name" id="color_name" value="{{old('color_name')}}"/>
+                                                <button type="button" class="btn btn-primary mt-2" id="add_color">Add More</button>
+
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-6 col-sm-12 col-12">
+                                            <div class="mb-3" id="size_fields">
+                                                <label for="size_name" class="form-label">Size (Optional)</label>
+                                                <input type="text" class="form-control mb-2" name="size_name[]" placeholder="Size Name" id="size_name" value="{{old('size_name')}}"/>
+                                                <button type="button" class="btn btn-primary mt-2" id="add_size">Add More</button>
+
                                             </div>
                                         </div>
 
                                         <div class="col-md-4 col-sm-6 col-6">
                                             <div class="mb-3">
                                                 <label for="buying_price" class="form-label">Product Buying Price</label>
-                                                <input type="number" class="form-control" name="buying_price" id="buying_price" required/>
+                                                <input type="number" class="form-control" name="buying_price" id="buying_price" value="{{old('buying_price')}}"/>
+                                                @error('buying_price')
+                                                     <span class="text-danger">{{$message}}</span>
+                                                 @enderror
                                             </div>
                                         </div>
 
                                         <div class="col-md-4 col-sm-6 col-6">
                                             <div class="mb-3">
                                                 <label for="regular_price" class="form-label">Product Regular Price</label>
-                                                <input type="number" class="form-control" name="regular_price" id="regular_price" required/>
+                                                <input type="number" class="form-control" name="regular_price" id="regular_price" value="{{old('regular_price')}}"/>
+                                                @error('regular_price')
+                                                     <span class="text-danger">{{$message}}</span>
+                                                 @enderror
                                             </div>
                                         </div>
 
                                         <div class="col-md-4 col-sm-6 col-6">
                                             <div class="mb-3">
                                                 <label for="discount_price" class="form-label">Product Discount Price (Optional)</label>
-                                                <input type="number" class="form-control" name="discount_price" id="discount_price"/>
+                                                <input type="number" class="form-control" name="discount_price" id="discount_price" value="{{old('discount_price')}}"/>
+                                                @error('discount_price')
+                                                     <span class="text-danger">{{$message}}</span>
+                                                 @enderror
                                             </div>
                                         </div>
 
                                         <div class="col-md-6 col-sm-12 col-12">
                                             <div class="mb-3">
                                                 <label for="qty" class="form-label">Product Quantity</label>
-                                                <input type="number" class="form-control" name="qty" id="qty" required/>
+                                                <input type="number" class="form-control" name="qty" id="qty" value="{{old('qty')}}"/>
+                                                @error('qty')
+                                                     <span class="text-danger">{{$message}}</span>
+                                                 @enderror
                                             </div>
                                         </div>
 
                                         <div class="col-md-6 col-sm-12 col-12">
                                             <div class="mb-3">
                                                 <label for="product_type" class="form-label">Product Type</label>
-                                                <select name="product_type" id="product_type" class="form-control" required>
+                                                <select name="product_type" id="product_type" class="form-control">
+                                                    <option value="" disabled selected>Select Product Type</option>
                                                     <option value="hot">Hot Product</option>
                                                     <option value="new">New Product</option>
                                                     <option value="regular">Regular Product</option>
                                                     <option value="discount">Discount Product</option>
                                                 </select>
+                                                @error('product_type')
+                                                     <span class="text-danger">{{$message}}</span>
+                                                 @enderror
                                             </div>
                                         </div>
 
                                         <div class="col-md-12">
                                             <div class="mb-3">
                                                 <label for="summernote" class="form-label">Product Description</label>
-                                                <textarea name="description" class="form-control" id="summernote" required></textarea>
+                                                <textarea name="description" class="form-control" id="summernote">{{old('description')}}</textarea>
+                                                @error('description')
+                                                     <span class="text-danger">{{$message}}</span>
+                                                 @enderror
                                             </div>
                                         </div>
 
                                         <div class="col-md-12">
                                             <div class="mb-3">
                                                 <label for="summernote_two" class="form-label">Product Policy (Optional)</label>
-                                                <textarea name="product_policy" class="form-control" id="summernote_two"></textarea>
+                                                <textarea name="product_policy" class="form-control" id="summernote_two">{{old('product_policy')}}</textarea>
+                                                @error('product_policy')
+                                                     <span class="text-danger">{{$message}}</span>
+                                                 @enderror
                                             </div>
                                         </div>
 
                                         <div class="col-md-12 col-sm-12 col-12">
                                             <div class="input-group mb-3">
                                                 <input type="file" class="form-control" name="image"
-                                                    id="inputGroupFile02" accept="image/*" required />
-                                                <label class="input-group-text" for="inputGroupFile02">Upload</label>
+                                                    id="image" accept="image/*" />
+                                                <label class="input-group-text" for="image">Main Image</label>
                                             </div>
+                                            @error('image')
+                                                <span class="text-danger">{{$message}}</span>
+                                            @enderror
+                                        </div>
+
+                                        <div class="col-md-12 col-sm-12 col-12">
+                                            <div class="input-group mb-3">
+                                                <input type="file" class="form-control" name="gallery_image[]"
+                                                    id="gallery_image" accept="image/*" multiple/>
+                                                <label class="input-group-text" for="gallery_image">Gallery Image</label>
+                                            </div>
+                                            @error('image')
+                                                <span class="text-danger">{{$message}}</span>
+                                            @enderror
                                         </div>
                                     </div>
                                 </div>
@@ -178,4 +245,23 @@
             $('#summernote_two').summernote();
         });
     </script>
+
+{{-- Add More Color --}}
+    <script>
+        $(document).ready(function(){
+            $("#add_color").click(function(){
+                $(this).before('<input type="text" class="form-control mb-2" name="color_name[]" placeholder="Color Name" id="color_name" value="{{old('color_name')}}"/>')
+            })
+        });
+    </script>
+
+    {{-- Add More Size --}}
+    <script>
+        $(document).ready(function(){
+            $("#add_size").click(function(){
+                $(this).before('<input type="text" class="form-control mb-2" name="size_name[]" placeholder="Size Name" id="size_name" value="{{old('size_name')}}"/>')
+            })
+        });
+    </script>
+
 @endpush
