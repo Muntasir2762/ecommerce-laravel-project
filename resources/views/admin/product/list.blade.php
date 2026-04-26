a827a5bdb4e604ef6b279098ad8fc085e096a4bd
Munotasir2762 <muntasirpranto2762@gmail.com>
2026-04-27 01:06:49 +0600

    Product CRUD is done

--- resources/views/admin/product/list.blade.php
+++ resources/views/admin/product/list.blade.php
@@ -74,8 +74,8 @@
                                                     @else
                                                         <a href="{{url('/manage/product-status/'.$product->id)}}" class=""><i class="fa-solid fa-circle-xmark text-danger" title="Inactive"></i></a>
                                                     @endif
-                                                    <a href="" class="text-primary"><i class="fa-solid fa-pen-to-square"></i></a>
-                                                    <a href="" class="text-danger" onclick="return confirm('Are you sure?')"><i class="fa-solid fa-trash"></i></a>
+                                                    <a href="{{url('/manage/product-edit/'.$product->id)}}" class="text-primary"><i class="fa-solid fa-pen-to-square"></i></a>
+                                                    <a href="{{url('/manage/product-delete/'.$product->id)}}" class="text-danger" onclick="return confirm('Are you sure?')"><i class="fa-solid fa-trash"></i></a>
                                                 </div>
                                             </td>
                                         </tr>
